f902384295afc1c65e61fb93cd4109c58e367464
pjohn1 <P_J_Johnson@hotmail.com>
2024-03-12 15:39:13 +0000

    Robust parking controller with error tracking; should be finished

--- visual_servoing/visual_servoing/parking_controller.py
+++ visual_servoing/visual_servoing/parking_controller.py
@@ -19,6 +19,8 @@ class ParkingController(Node):
         self.declare_parameter("drive_topic")
         DRIVE_TOPIC = self.get_parameter("drive_topic").value # set in launch file; different for simulator vs racecar
 
+        self.parking_distance = 0.75 # meters; try playing with this number!
+
         self.drive_pub = self.create_publisher(AckermannDriveStamped, DRIVE_TOPIC, 10)
         self.error_pub = self.create_publisher(ParkingError, "/parking_error", 10)
 
@@ -27,11 +29,22 @@ class ParkingController(Node):
         
         self.MAX_TURN = .34
 
-        self.parking_distance = .75 # meters; try playing with this number!
+        self.get_logger().info(str(self.parking_distance))
         self.relative_x = 0
         self.relative_y = 0
+        self.dist = 0
+
+        try: #initialize a variable but do not update it in spin
+            if not self.distance_check:
+                #do not change variable value if it has been initialized already
+                self.distance_check = False
+        except AttributeError:
+            self.distance_check = False
 
         self.get_logger().info("Parking Controller Initialized")
+
+    def error(self,actual,desired):
+        return (desired - actual)/actual
 
 
     def relative_cone_callback(self, msg):
@@ -39,49 +52,66 @@ class ParkingController(Node):
         self.relative_y = msg.y_pos
         drive_cmd = AckermannDriveStamped()
 
-        kx = 1
-        ky = 1
         ka = 1
-        x_des = 0
-        y_des = 0
         angle_des = 0
 
         kp = 1/3
 
-        dist = ( self.relative_x**2 + self.relative_y**2 ) ** (1/2)
+        self.dist = ( self.relative_x**2 + self.relative_y**2 ) ** (1/2)
         angle = np.arctan2(self.relative_y,self.relative_x)
+
         eps = 0.1
 
-        if abs(dist - self.parking_distance) < eps and abs(angle-angle_des) < eps:
+        if abs(self.dist - self.parking_distance) < eps and abs(angle-angle_des) < eps:
+            #goal check
             speed = 0.0
             turning_angle = 0.0
         else:
-            # Px = -kx * (x_des-self.relative_x)
-            # Py = -ky * (y_des-self.relative_y)
-
-            P = self.parking_distance - dist
-
+            P = self.parking_distance - self.dist
             Pangle = -ka * (angle_des - angle)
 
-            turning_angle = Pangle
-            speed = -kp * P
+            if P>0 or self.distance_check:
+                #if P>0, we are too close to the cone and may need to back up
+                #if self.distance_check is true, we are already backing up and need to check distance
+                
+                if self.distance_check:
+                    
+                    if self.dist < 1.5*self.parking_distance:
+                        #if we have not backed up far enough, continue
+                        speed = float(-1)
+                        turning_angle = -Pangle
 
-            if P < 0 and self.relative_x < 0:
-                speed = -speed
-                turning_angle = -turning_angle
+                    else:
+                        #if we have backed up far enough, set distance check to false
+                        #and resume regular control
+                        self.distance_check = False
+                        speed = 0.0
+                        turning_angle = 0.0
+                
+                else:
+                    #if we are too close but have not started backing up
+                    #set distance check to true to enable the backup sequence
+                    self.distance_check = True
+                    speed = 0.0
+                    turning_angle = 0.0
 
-            if abs(speed) < 1:
-                speed = float(1) if speed > 0 else float(-1)
 
-            self.get_logger().info(str(speed)+','+str(turning_angle)+','+str(self.relative_x))
-            # self.get_logger().info(str(dist))
+            else:
+                turning_angle = Pangle
+                speed = -kp * P
 
-            # # if dist < circle_radius and Pangle > 0.5:
-            # #     self.get_logger().info('driving back')
-            # #     speed = -speed
-            # if self.relative_x < 0:
-            #     speed = -speed
-            #     turning_angle = -turning_angle
+                if P < 0 and self.relative_x < 0:
+                    #if the cone is behind us, go backward instead of forward
+                    speed = -speed
+                    turning_angle = -turning_angle
+
+                if abs(speed) < 1:
+                    #this statement may need tuning on actual robot depending on the
+                    #motor issues that we faced
+                    speed = float(1) if speed > 0 else float(-1)
+                    
+        if abs(turning_angle) > self.MAX_TURN:
+            turning_angle = self.MAX_TURN if turning_angle > 0 else -self.MAX_TURN
 
 
 
@@ -98,12 +128,16 @@ class ParkingController(Node):
         """
         error_msg = ParkingError()
 
-        #################################
+        x_des = self.parking_distance
+        y_des = 0.0
 
-        # YOUR CODE HERE
-        # Populate error_msg with relative_x, relative_y, sqrt(x^2+y^2)
+        x_error = self.error(self.relative_x,x_des)
+        y_error = self.error(self.relative_y,y_des)
+        dist_error = self.error(self.dist,self.parking_distance)
 
-        #################################
+        error_msg.x_error = x_error
+        error_msg.y_error = y_error
+        error_msg.distance_error = dist_error
         
         self.error_pub.publish(error_msg)
 
