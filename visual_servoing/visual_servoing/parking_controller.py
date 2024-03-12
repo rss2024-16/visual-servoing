8a831635343740ee5b0c5be9afcae2d9fd945145
pjohn1 <P_J_Johnson@hotmail.com>
2024-03-12 02:42:32 +0000

    Parking works in cases where cone starts outside of parking distance circle; need to add error publishing

--- visual_servoing/visual_servoing/parking_controller.py
+++ visual_servoing/visual_servoing/parking_controller.py
@@ -24,6 +24,8 @@ class ParkingController(Node):
 
         self.create_subscription(ConeLocation, "/relative_cone", 
             self.relative_cone_callback, 1)
+        
+        self.MAX_TURN = .34
 
         self.parking_distance = .75 # meters; try playing with this number!
         self.relative_x = 0
@@ -31,17 +33,60 @@ class ParkingController(Node):
 
         self.get_logger().info("Parking Controller Initialized")
 
+
     def relative_cone_callback(self, msg):
         self.relative_x = msg.x_pos
         self.relative_y = msg.y_pos
         drive_cmd = AckermannDriveStamped()
 
-        #################################
+        kx = 1
+        ky = 1
+        ka = 1
+        x_des = 0
+        y_des = 0
+        angle_des = 0
 
-        # YOUR CODE HERE
-        # Use relative position and your control law to set drive_cmd
+        kp = 1/3
 
-        #################################
+        dist = ( self.relative_x**2 + self.relative_y**2 ) ** (1/2)
+        angle = np.arctan2(self.relative_y,self.relative_x)
+        eps = 0.1
+
+        if abs(dist - self.parking_distance) < eps and abs(angle-angle_des) < eps:
+            speed = 0.0
+            turning_angle = 0.0
+        else:
+            # Px = -kx * (x_des-self.relative_x)
+            # Py = -ky * (y_des-self.relative_y)
+
+            P = self.parking_distance - dist
+
+            Pangle = -ka * (angle_des - angle)
+
+            turning_angle = Pangle
+            speed = -kp * P
+
+            if P < 0 and self.relative_x < 0:
+                speed = -speed
+                turning_angle = -turning_angle
+
+            if abs(speed) < 1:
+                speed = float(1) if speed > 0 else float(-1)
+
+            self.get_logger().info(str(speed)+','+str(turning_angle)+','+str(self.relative_x))
+            # self.get_logger().info(str(dist))
+
+            # # if dist < circle_radius and Pangle > 0.5:
+            # #     self.get_logger().info('driving back')
+            # #     speed = -speed
+            # if self.relative_x < 0:
+            #     speed = -speed
+            #     turning_angle = -turning_angle
+
+
+
+        drive_cmd.drive.speed = speed
+        drive_cmd.drive.steering_angle = turning_angle
 
         self.drive_pub.publish(drive_cmd)
         self.error_publisher()
@@ -65,7 +110,12 @@ class ParkingController(Node):
 def main(args=None):
     rclpy.init(args=args)
     pc = ParkingController()
-    rclpy.spin(pc)
+    try:
+        rclpy.spin(pc)
+    except KeyboardInterrupt:
+        msg = AckermannDriveStamped()
+        msg.drive.speed = 0.0
+        pc.drive_pub.publish(msg)
     rclpy.shutdown()
 
 if __name__ == '__main__':
