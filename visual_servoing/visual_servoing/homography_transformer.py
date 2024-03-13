fcdfbbde3538574c451aff16d2057da1e47eb986
Phillip Johnson <P_J_Johnson@hotmail.com>
2024-03-13 17:15:18 -0400

    homography

--- visual_servoing/visual_servoing/homography_transformer.py
+++ visual_servoing/visual_servoing/homography_transformer.py
@@ -57,7 +57,7 @@ class HomographyTransformer(Node):
         self.marker_pub = self.create_publisher(Marker, "/cone_marker", 1)
         self.cone_px_sub = self.create_subscription(ConeLocationPixel, "/relative_cone_px", self.cone_detection_callback, 1)
 
-        self.pixel_sub = self.create_subscription(Point, "/zed/rgb/image_rect_color_mouse_left", self.image_callback, 10)
+        self.pixel_sub = self.create_subscription(Point, "/zed/zed_node/rgb/image_rect_color_mouse_left", self.image_callback, 10)
 
         if not len(PTS_GROUND_PLANE) == len(PTS_IMAGE_PLANE):
             rclpy.logerr("ERROR: PTS_GROUND_PLANE and PTS_IMAGE_PLANE should be of same length")
@@ -139,7 +139,8 @@ class HomographyTransformer(Node):
         '''
         u, v, z = msg.x, msg.y, msg.z
         x, y = self.transformUvToXy(u, v)
-        self.draw_marker(x, y, msg.header.frame_id)
+        self.get_logger().info('x:' + str(x) + 'y:' + str(y))
+        self.draw_marker(x, y, 'map')
 
 def main(args=None):
     rclpy.init(args=args)
